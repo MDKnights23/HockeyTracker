a457957f6ca6eb1a03f85d6df9221a6f5088b61c
MDKnights23 <67720886+MDKnights23@users.noreply.github.com>
2022-08-09 10:59:01 -0500

    Update ui.R

--- Hockey Tracker/ui.R
+++ Hockey Tracker/ui.R
@@ -4,31 +4,36 @@ library(ggpubr)
 library(shiny)
 
 # Table more 'centered'
-ui <- pageWithSidebar(
+ui <- fluidPage(
   headerPanel("Hockey Tracker"),
   sidebarPanel(
     # Change radio button info to Team
-    radioButtons("color", "Team", c("Pink", "Blue")),
+    radioButtons("team", "Team", c("Pink", "Blue")),
     
     # Add numericInput field for player number
-    numericInput("player", "Player Number", 50, min = 0, max = 99),
+    numericInput("player", "Player Number", 10, min = 0, max = 99),
     
     # Change selectInput to Event
     selectInput("shape", "Event", c("Circle", "Triangle"))
     
   ),
   mainPanel(
-      fluidRow(column(12,
-                      h4("Click to place event location"),
-                      actionButton("rem_point", "Remove Last Point"),
-                      plotOutput("plot1", click = "plot_click",
-                      # Set fixed values so plot does not 'stretch'
-                      width = "600px", height = "300px"))
-  ),
-  fluidRow(column(12,
-                  h4("Table of points on plot"),
-                  downloadLink("download"),
-                  tableOutput("table"))
-  )
+    fluidRow(column(12,
+                    h4("Click to place event location"),
+                    actionButton("rem_point", "Remove Last Point"),
+                    plotOutput(
+                      
+                               "plot1", click = "plot_click",
+                               # Set fixed values so plot does not 'stretch'
+                               width = "800px", height = "340px"
+                               
+                               )
+                    )
+    ),
+    fluidRow(column(12,
+                    h4("Table of points on plot"),
+                    downloadLink("download"),
+                    tableOutput("table"))
+    )
   )
 )
