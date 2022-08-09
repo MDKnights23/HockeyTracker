a6be177373eea93375f03f3762d60a84a3db2a2d
MDKnights23 <67720886+MDKnights23@users.noreply.github.com>
2022-08-09 10:58:34 -0500

    Update server.R

--- Hockey Tracker/server.R
+++ Hockey Tracker/server.R
@@ -8,21 +8,27 @@ server <- function(input, output, session){
   
   ## 1. set up reactive dataframe ##
   values <- reactiveValues()
-  values$DT <- data.frame(x = numeric(),
-                          y = numeric(),
-                          color = factor(),
-                          shape = factor())
+  values$DT <- data.frame(
+    
+                          team = factor(),
+                          number = numeric(),
+                          shape = factor(),
+                          x = numeric(),
+                          y = numeric()
+                          
+                          
+                          )
   
   ## 2. Create a plot ##
   output$plot1 = renderPlot({
     ggplot(values$DT, aes(x = x, y = y)) +
       #This must be on top of the points in the code or the points drawn behind    
       background_image(img) + 
-      geom_point(aes(color = color,
+      geom_point(aes(color = team,
                      shape = shape), size = 5) +
       lims(x = c(0, 200), y = c(-44.5, 44.5)) +
       theme(legend.position = "none") +
-          
+      
       # include so that colors don't change as more color/shape chosen
       scale_color_discrete(drop = FALSE) +
       scale_shape_discrete(drop = FALSE)
@@ -32,10 +38,15 @@ server <- function(input, output, session){
   observeEvent(input$plot_click, {
     # each input is a factor so levels are consistent for plotting characteristics
     add_row <- 
-        data.frame(x = input$plot_click$x,
-                          y = input$plot_click$y,
-                          color = factor(input$color, levels = c("Pink", "Blue")),
-                          shape = factor(input$shape, levels = c("Circle", "Triangle")))
+      data.frame(
+        
+                 team = factor(input$team, levels = c("Pink", "Blue")),
+                 number = input$player,
+                 shape = factor(input$shape, levels = c("Circle", "Triangle")),
+                 x = input$plot_click$x,
+                 y = input$plot_click$y
+                 
+                 )
     # add row to the data.frame
     values$DT <- rbind(values$DT, add_row)
   })
